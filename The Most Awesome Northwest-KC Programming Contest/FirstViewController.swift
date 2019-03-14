7a5ba4587898d64c97c6b9a5cae8638f0768987a
Injamuri,Satyavrath <s533983@ol10036m.nwmissouri.edu>
2019-03-14 11:55:42 -0500

    Initial Commit

---
+++ The Most Awesome Northwest-KC Programming Contest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  The Most Awesome Northwest-KC Programming Contest
+//
+//  Created by Injamuri,Satyavrath on 3/14/19.
+//  Copyright © 2019 Injamuri,Satyavrath. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
